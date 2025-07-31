5ac40b131b8cfc7edb5956bde068450cc2ffe2ce
Samuel Mena <Samuelmena2406@gmail.com>
2025-07-31 10:02:38 -0500

    Lista Url Apis

---
+++ Api/obtener_url.py
@@ -0,0 +1,10 @@
+#Url Para las Apis
+
+ #Hay que automatizar las fechas
+
+Url = {
+    "listar_factura" : "https://api.siigo.com/v1/invoices?created_start=2021-02-17",
+    "listar_froductos" : "https://api.siigo.com/v1/products?created_start=2021-02-17",
+    "listar_flientes" : "https://api.siigo.com/v1/customers?created_start=2021-02-17"
+}
+
